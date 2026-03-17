2bbe16fb1dc28044399df1c89e95ca55b20bd3f7
JmpKey <62756042+JmpKey@users.noreply.github.com>
2026-03-17 15:01:46 +0300

    Rewrite and add args [visit.py]

--- visit.py
+++ visit.py
@@ -1,36 +1,53 @@
 import os
+import shutil
 import git
+import argparse
 
-# parameters
-repo_url = 'https://your.site/user/repo'  # URL repo
-local_repo_path = 'repo/dir'  # local repository path
-file_name = 'dir/list.txt'  # file to read
-search_string = ':1080'  # type the substring you want to search for
+# args
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Search for a substring in a file from a Git repo.')
+    parser.add_argument('-r', '--repo', required=True, help='URL repo')
+    parser.add_argument('-f', '--file', required=True, help='Path to the file to read relative to the repo')
+    parser.add_argument('-s', '--search', required=True, help='Substring to search for')
+    return parser.parse_args()
 
-# git clone and update
-if not os.path.exists(local_repo_path):
+def main():
+    args = parse_arguments()
+    
+    # param
+    repo_url = args.repo  # URL repo
+    local_repo_path = 'repo/dir'  # local repository path
+    file_name = args.file  # file read
+    search_string = args.search  # substring for search
+
+    # clear dir
+    if os.path.exists(local_repo_path):
+        print(f"Removing existing directory: {local_repo_path}")
+        shutil.rmtree(local_repo_path)
+
+    # clone
     print("Cloning...")
     git.Repo.clone_from(repo_url, local_repo_path)
-else:
-    print("Update...")
-    repo = git.Repo(local_repo_path)
-    repo.remotes.origin.pull()
 
-file_path = os.path.join(local_repo_path, file_name)
-first_record = None
+    file_path = os.path.join(local_repo_path, file_name)
 
-if os.path.exists(file_path):
-    with open(file_path, 'r') as file:
-        lines = file.readlines()
+    if os.path.exists(file_path):
+        found_records = []
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
 
-        for line in reversed(lines):
-            if search_string in line:
-                first_record = line.strip()
-                break
+            for line in reversed(lines):
+                if search_string in line:
+                    found_records.append(line.strip())
 
-    if first_record:
-        print(f'Return "{search_string}": {first_record}')
+        if found_records:
+            print(f'Found occurrences of "{search_string}":')
+            for record in found_records:
+                print(record)
+        else:
+            print('No occurrences found')
     else:
-        print('No found')
-else:
-    print(f'File {file_path} no found')
+        print(f'File {file_path} not found')
+
+if __name__ == '__main__':
+    main()
